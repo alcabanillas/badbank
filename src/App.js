c3bcc72fced9ad5e18c1d5561cc61d4dfb0fe009
alcabanillas <acabanillas@gmail.com>
2022-11-17 01:25:55 +0100

    reestructurando componentes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import { NavBar } from "./routes/navbar";
-import { Home } from "./components/home";
-import { CreateAccount } from "./components/createaccount";
-import { Login } from "./components/login";
-import { Deposit } from "./components/deposit";
-import { WithDraw } from "./components/withdraw";
-import { Balance } from "./components/balance";
-import { AllData } from "./components/alldata";
+import { Home } from "./pages/home";
+import { CreateAccount } from "./pages/createaccount";
+import { Login } from "./pages/login";
+import { Deposit } from "./pages/deposit";
+import { WithDraw } from "./pages/withdraw";
+import { AllData } from "./pages/alldata";
 
 //import "bootstrap/dist/css/bootstrap.css";
 import "bootswatch/dist/flatly/bootstrap.min.css";
