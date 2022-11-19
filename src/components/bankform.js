906339b39b7d3968fba26dc026229c069122ddde
alcabanillas <acabanillas@gmail.com>
2022-11-19 19:59:42 +0100

    add password eye

--- src/components/bankform.js
+++ src/components/bankform.js
@@ -1,12 +1,14 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { Formik } from "formik";
+import { InputGroup } from "react-bootstrap";
+import {useState} from "react"
 
-function BankForm({buttonSubmit,
-  handle,
-  fields,
-  initialData,
-  schema}) {
+function BankForm({ buttonSubmit, handle, fields, initialData, schema }) {
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePasswordVisiblity = () => {
+    setPasswordShown(passwordShown ? false : true);
+  };
 
   return (
     <Formik
@@ -14,36 +16,41 @@ function BankForm({buttonSubmit,
       onSubmit={handle}
       initialValues={initialData}
     >
-      {({
-        handleSubmit,
-        handleChange,
-        touched,
-        isValid,
-        errors,
-        dirty
-      }) => (
+      {({ handleSubmit, handleChange, touched, isValid, errors, dirty }) => (
         <Form className="mb-3" noValidate onSubmit={handleSubmit}>
           {fields.map((elem, index) => {
+            let isPassword = (elem.type == 'password')
+
             return (
               <Form.Group className="mb-3" key={index}>
                 <Form.Label htmlFor={elem.id}>{elem.id}</Form.Label>
-                <Form.Control
-                  type={elem.type}
-                  key={index}
-                  id={elem.id}
-                  onChange={handleChange}
-                  placeholder={elem.placeholder}
-                  isValid={touched[elem.id] && !errors[elem.id]}
-                  isInvalid={!!errors[elem.id]}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {errors[elem.id]}
-                </Form.Control.Feedback>
-            </Form.Group>              
-            )
+                <InputGroup hasValidation>
+                  <Form.Control
+                    type={isPassword ? (passwordShown ? "text" : "password") : elem.type}
+                    key={index}
+                    id={elem.id}
+                    onChange={handleChange}
+                    placeholder={elem.placeholder}
+                    isValid={touched[elem.id] && !errors[elem.id]}
+                    isInvalid={!!errors[elem.id]}
+                  />
+                  {isPassword && <Button>
+                  <i className={ passwordShown ? "bi bi-eye" : "bi bi-eye-slash"} onClick={togglePasswordVisiblity}></i>
+                  </Button>}
+                  <Form.Control.Feedback type="invalid">
+                    {errors[elem.id]}
+                  </Form.Control.Feedback>
+                </InputGroup>
+              </Form.Group>
+            );
           })}
           <div className="text-center">
-            <Button disabled={!(isValid && dirty)} type="submit" className="btn btn-primary" data-testid={`btn${buttonSubmit.replace(" ", "")}`}>
+            <Button
+              disabled={!(isValid && dirty)}
+              type="submit"
+              className="btn btn-primary"
+              data-testid={`btn${buttonSubmit.replace(" ", "")}`}
+            >
               {buttonSubmit}
             </Button>
           </div>
@@ -52,6 +59,5 @@ function BankForm({buttonSubmit,
     </Formik>
   );
 }
-
 
 export default BankForm;
